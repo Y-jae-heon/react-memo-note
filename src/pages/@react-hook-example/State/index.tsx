b6474ddd53ba0b28ff2b28bfec03c57e69e05b56
Yeom Jaeheon <121541876+Y-jae-heon@users.noreply.github.com>
2024-03-14 12:20:50 +0900

    chore: console 설명문 추가

--- src/pages/@react-hook-example/State/index.tsx
+++ src/pages/@react-hook-example/State/index.tsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 
 function State() {
   const [state, setState] = useState({ count: 0 });
-  console.log("<<<<<<< rerendering");
+  console.log("State Hook이 prev를 반환할 시 Rerendering 일어나지 않음");
+  console.log(
+    "State Hook이 Object 또는 Array 일 시 같은 값이여도 Rerendering이 일어남"
+  );
   return (
     <div>
       {state.count}
